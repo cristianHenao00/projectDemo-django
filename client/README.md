4ebd72816c64797ca24971fa109a1fa85383dab8
CristianHenao <cristianhenao88@gmail.com>
2023-10-15 17:25:13 -0500

    Frontend configured with tailwind framework.

--- client/README.md
+++ client/README.md
@@ -6,3 +6,24 @@ Currently, two official plugins are available:
 
 - [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+
+## Usage
+
+```bash
+yarn create vite my-react-app --template react
+# or
+npm init vite my-react-app --template react
+```
+
+## Install dependencies
+
+```bash
+cd my-react-app
+yarn add # or npm install
+```
+
+## Start dev server
+
+```bash
+yarn dev # or npm run dev
+```
--- client/src/pages/TasksFormPage.jsx
+++ client/src/pages/TasksFormPage.jsx
@@ -2,6 +2,7 @@ import { useForm } from "react-hook-form";
 import { createTask, getTask, deleteTask, updateTask } from "../api/tasks.api";
 import { useNavigate, useParams } from "react-router-dom";
 import { useEffect } from "react";
+import { toast } from "react-hot-toast";
 
 export function TasksFormPage() {
   const {
@@ -16,8 +17,10 @@ export function TasksFormPage() {
   const onSubmit = handleSubmit(async (data) => {
     if (!params.id) {
       await createTask(data);
+      toast.success("Task New created");
     } else {
       await updateTask(params.id, data);
+      toast.success("Task updated");
     }
     navigate("/tasks");
   });
@@ -28,18 +31,20 @@ export function TasksFormPage() {
         const { data } = await getTask(params.id);
         setValue("title", data.title);
         setValue("description", data.description);
+        toast.success("Task loaded");
       }
     }
     loadTask();
   }, []);
 
   return (
-    <div>
-      <form onSubmit={onSubmit}>
+    <div className="max-w-xl mx-auto">
+      <form onSubmit={onSubmit} className="bg-zinc-800 p-10 rounded-lg mt-2">
         <input
           type="text"
           placeholder="Title"
           {...register("title", { required: true })}
+          className="bg-zinc-700 p-3 rounded-lg block w-full mb-3"
           autoFocus
         />
         {errors.title && <span>This field is required</span>}
@@ -47,24 +52,31 @@ export function TasksFormPage() {
           rows="3"
           placeholder="Description"
           {...register("description", { required: true })}
+          className="bg-zinc-700 p-3 rounded-lg block w-full"
         ></textarea>
         {errors.title && <span>This field is required</span>}
-        <button>Save</button>
+        <button className="bg-indigo-500 p-3 rounded-lg block w-full mt-3">
+          Save
+        </button>
       </form>
       {params.id && (
-        <button
-          onClick={async () => {
-            const accepted = window.confirm(
-              "Are you sure you want to delete this task?"
-            );
-            if (accepted) {
-              await deleteTask(params.id);
-              navigate("/tasks");
-            }
-          }}
-        >
-          Delete
-        </button>
+        <div className="flex justify-end">
+          <button
+            className="bg-red-500 p-3 rounded-lg w-48 mt-3"
+            onClick={async () => {
+              const accepted = window.confirm(
+                "Are you sure you want to delete this task?"
+              );
+              if (accepted) {
+                await deleteTask(params.id);
+                toast.success("Task deleted");
+                navigate("/tasks");
+              }
+            }}
+          >
+            Delete
+          </button>
+        </div>
       )}
     </div>
   );
